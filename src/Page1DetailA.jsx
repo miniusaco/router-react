ae184eb94035485f6119e6692e8773a6f373589f
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-04-17 13:48:53 +0900

    stateのエラー対応

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -2,7 +2,10 @@ import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  console.log(state);
+
   const history = useHistory();
+
   const onClickBack = () => history.goBack();
 
   return (
